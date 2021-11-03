1dca94b8ac7ecae7174912ab614ff06e7b099ac3
Fernando <fhomaia@gmail.com>
2021-11-03 15:52:13 -0300

    adiciona lint e arquivo server

---
+++ Back-end/server.js
@@ -0,0 +1,18 @@
+require('dotenv/config');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json())
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: `http://localhost:${PORT}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
