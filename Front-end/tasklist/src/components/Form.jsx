8a2bc86b86cc79094ce0038041a76d6b284d4647
Fernando <fhomaia@gmail.com>
2021-11-04 00:20:49 -0300

    cria componentes TarskCard

--- Front-end/tasklist/src/components/Form.jsx
+++ Front-end/tasklist/src/components/Form.jsx
@@ -3,30 +3,46 @@ import { io } from 'socket.io-client';
 
 function Form() {
   const socket = io();
-  const [task, setTask] = useState({ task: '', status: 'Backlog'});
-  const onInputChange = ({ target: { value }}) => setTask({ ...task, task: value });
-  const onSelectChange = ({ target: { value }}) => setTask({ ...task, status: value });
-  const onClickButton = () => socket.emit('createTask', task);
+  const [task, setTask] = useState({ task: '', status: 'Backlog', dueDate:''});
+  const onChangeTask = ({ target: { value }}) => setTask({ ...task, task: value });
+  const onChangeStatus = ({ target: { value }}) => setTask({ ...task, status: value });
+  const onChangeDueDate = ({ target: { value }}) => setTask({ ...task, dueDate: value });
+  const onClickCreate = () => socket.emit('createTask', task);
+  const disableCreate = () => {
+    console.log('aquiii')
+    if (!task.task || !task.dueDate) return true;
+    return false;
+  };
+
   console.log(task)
   return (
     <form>
       <label htmlFor='task'>
-        <input
+        <textarea
         id='task'
         placeholder='Insert a task'
-        onChange={(event) => onInputChange(event)}
+        onChange={(event) => onChangeTask(event)}
         />
       </label>
       <label htmlFor='statusbox'>
         Status
-      <select id='statusbox' onChange={(event) => onSelectChange(event)} >
+      <select id='statusbox' onChange={(event) => onChangeStatus(event)} >
         <option value='Backlog'>Backlog</option>
         <option value='In-Progress'>In Progress</option>
         <option value='Testing'>Testing</option>
         <option value='Done'>Done</option>
       </select>
       </label>
-      <button type='button' onClick={onClickButton} >Create</button>
+      <label>
+        Due Date: <input type='date' onChange={(event) => onChangeDueDate(event)}/>
+      </label>
+      <button
+        type='button'
+        onClick={onClickCreate}
+        disabled={disableCreate()}
+      >
+        Create
+      </button>
     </form>
   );
 }
--- Front-end/tasklist/src/components/TaskCard.jsx
+++ Front-end/tasklist/src/components/TaskCard.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { io } from 'socket.io-client';
 
 function TaskCard(task) {
-  const { message } = task
+  const { _id, message, dueDate } = task;
+  const [edit, setEdit] = useState(false);
+  const [updateTask, setUpdateTask] = useState(task);
+  const socket = io('http://localhost:3001');
+  const onClickDelete = socket.emit('deletTask', _id);
+  const editMode = () => setEdit(!edit);
+
+  const onChangeMessage = ({ target: { value }}) => (
+    setUpdateTask({ ...updateTask, message: value })
+  );
+
+  const onChangeDueDate = ({ target: { value }}) => (
+    setUpdateTask({ ...updateTask, dueDate: value })
+  );
+
+  const onClickConfirm = socket.emit('updateTask', task);
+
+  if (edit) {
+    return (
+      <div>
+      <button type='button' onClick={onClickDelete} >X</button>
+      <button type='button' onClick={editMode} >✏️</button>
+      <textarea value={ updateTask.message } onChange={(e) => onChangeMessage(e) } />
+      <input value={ updateTask.dueDate } onChange={(e) => onChangeDueDate(e) } />
+      <button type='button'>&larr;</button>
+      <button type='button'>&rarr;</button>
+      <button type='button' onClick={ onClickConfirm } >Confirm</button>
+      </div>
+    )
+  }
   return (
-    <div>
-      <h3>{message}</h3>
-    </div>
+      <div>
+      <button type='button' onClick={ onClickDelete } >X</button>
+      <button type='button' onClick={ editMode } >✏️</button>
+      <h3>{ message }</h3>
+      <p>Due: { dueDate }</p>
+      <button type='button' >&larr;</button>
+      <button type='button' >&rarr;</button>
+      </div>
   )
 }
 
