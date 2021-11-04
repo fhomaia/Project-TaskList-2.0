ee0e33e0fe991f4730bf45d9b898240abeb6ba84
Fernando <fhomaia@gmail.com>
2021-11-04 14:52:53 -0300

    arquivos não adicionados no ultimo commit

--- Front-end/tasklist/src/components/TaskCard.jsx
+++ Front-end/tasklist/src/components/TaskCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import PropTypes from 'prop-types';
 
@@ -27,6 +27,31 @@ function TaskCard(props) {
     changeEditMode();
   };
 
+  const disableLeftArrow = (status) => {
+    if (status === 'Backlog') return true;
+    return false;
+  }
+
+  const disableRightArrow = (status) => {
+    if (status === 'Done') return true;
+    return false;
+  }
+
+  const moveTaskUpwards = (status) => {
+    const statusArray = ['Backlog', 'In-Progress', 'Testing','Done'];
+    const oldStatusPosition = statusArray.indexOf(status);
+    setUpdateTask({ ...updateTask, status: statusArray[oldStatusPosition + 1] });
+  }
+
+ useEffect(() => socket.emit('updateTask', updateTask), [updateTask.status])
+
+  const moveTaskDownwards = (status) => {
+    const statusArray = ['Backlog','In-Progress', 'Testing', 'Done'];
+    const oldStatusPosition = statusArray.indexOf(status);
+    setUpdateTask({ ...updateTask, status: statusArray[oldStatusPosition - 1] });
+    socket.emit('updateTask', updateTask);
+  }
+
   if (edit) {
     return (
       <div>
@@ -34,8 +59,20 @@ function TaskCard(props) {
       <button type='button' onClick={ changeEditMode } >✏️</button>
       <textarea value={ updateTask.task } onChange={ (e) => onChangeMessage(e) } />
       <input value={ updateTask.dueDate } type='date' onChange={(e) => onChangeDueDate(e) } />
-      <button type='button'>&larr;</button>
-      <button type='button'>&rarr;</button>
+      <button
+        type='button'
+        disabled={ disableLeftArrow(updateTask.status) }
+        onClick={() => moveTaskDownwards(updateTask.status)}
+      >
+        &larr;
+      </button>
+      <button
+        type='button'
+        disabled={ disableRightArrow(updateTask.status) }
+        onClick={() => moveTaskUpwards(updateTask.status)}
+      >
+        &rarr;
+      </button>
       <button type='button' onClick={ onClickConfirm } >Confirm</button>
       </div>
     )
@@ -46,8 +83,20 @@ function TaskCard(props) {
       <button type='button' onClick={ changeEditMode } >✏️</button>
       <h3>{ task }</h3>
       <p>Due: { dueDate }</p>
-      <button type='button' >&larr;</button>
-      <button type='button' >&rarr;</button>
+      <button
+        type='button'
+        disabled={ disableLeftArrow(updateTask.status) }
+        onClick={() => moveTaskDownwards(updateTask.status)}
+      >
+        &larr;
+      </button>
+      <button
+        type='button'
+        disabled={ disableRightArrow(updateTask.status) }
+        onClick={() => moveTaskUpwards(updateTask.status)}
+      >
+        &rarr;
+      </button>
       </div>
   )
 }
